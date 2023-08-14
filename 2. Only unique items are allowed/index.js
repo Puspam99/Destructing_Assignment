0e371f831e5461da150fc9a01f149dd98c4b961b
Puspam99 <resrishu99@gmail.com>
2023-08-14 22:06:16 +0530

    update

--- 2. Only unique items are allowed/index.js
+++ 2. Only unique items are allowed/index.js
@@ -1,3 +1,15 @@
+// 02. Only unique items are allowed.
+
+/* You are building a program that takes an array of numbers as input and you need to remove all the 
+duplicates from the array. You want to write a function that can accomplish this task efficiently 
+and returns a new set that contains only the unique elements.*/
+
+
+
+
+
+
+
 const arr = [1,2,3,4,5,6,4,3]
 
 function uniqueNum(arr){
